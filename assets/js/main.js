58030eb84d77f80e86b811aa65a6163a35a8ce59
Kamil <kamil@kamil.com>
2018-07-23 18:44:03 +0200

    new navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-    $('#nav-icon3').on('click', function (e) {
-        var collapse = $(".navbar-collapse");
-        e.preventDefault();
-        if (collapse.hasClass("collapsing")) {
-            e.preventDefault();
-        } else {
-            if (collapse.hasClass("show")) {
-                $(this).removeClass('open');
-            } else {
-                $(this).addClass('open');
-            }
-        }
-    });
-
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
 
