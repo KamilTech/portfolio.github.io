88abf7033a0224f63fc28b6d295dd1db76c7aaa5
Kamil <kamil@kamil.com>
2018-09-20 00:20:05 +0200

    page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
-    progressively.init();
+    // Configuration for ScrollReveal
+    window.sr = ScrollReveal();
+    sr.reveal('.anim-r', {
+        origin: 'right',
+        delay: 200
+    });
+    sr.reveal('.anim-li', {
+        delay: 500,
+        origin: 'right'
+    }, 100);
+    sr.reveal('.anim-b', {
+        origin: 'bottom',
+        delay: 200
+    });
+
+    progressively.init({
+        delay: 50,
+        throttle: 400
+    });
+
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
 
